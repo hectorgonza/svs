f28542c61632595b2bde1b59cc9ce2a4178d667d
hectorgonza <43784878+hectorgonza@users.noreply.github.com>
2023-01-02 20:09:09 +0100

    fixing router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,41 +1,24 @@
-import { Routes,  Route, Navigate, useLocation  } from 'react-router-dom';
+import { Routes,  Route, Navigate, Outlet } from 'react-router-dom';
 import Auth from './Metamask/Auth';
 import Home from './Views/Home';
 import LoginPage from './Metamask/LoginPage';
 
-function PrivateRoute({ component: Component, ...rest }) {
-  return (
-    <Route
-      {...rest}
-      render={props =>
-        Auth.isLoggedIn ? (
-          <Component {...props} />
-        ) : (
-          <Navigate to="/login" />
-        )
-      }
-    />
-  );
+
+function PrivateRoute() {
+  return  Auth.isLoggedIn ? <Home/>: <Navigate to="/login" />
+        
+
 }
 
 export default function App() {
-  const location = useLocation();
+  
 
     return (
       <Routes>
-        <Route exact path="/login">
-          <LoginPage />
-        </Route>
-        <PrivateRoute exact path="/">
-          <Home />
-        </PrivateRoute>
-        <PrivateRoute exact path="/home" component={Home} />
-        <Navigate from="/" to="/home" />
-      
-        {console.log(location.pathname)}
+        
+        <Route path="login" element={<LoginPage/>} ></Route>
+        <Route  path="/" element={<PrivateRoute/>} ></Route>
       </Routes>
-    );
-  
-    
+    )
 
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,13 +5,34 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 import './App.scss';
-import { Router } from 'react-router-dom';
+import {
+      BrowserRouter,
+      createBrowserRouter,
+      RouterProvider,
+    } from "react-router-dom";
+import Home from './Views/Home';
+import LoginPage from './Metamask/LoginPage';
+
+
+//     const router = createBrowserRouter([
+//       {
+//         path: "/",
+//         element: <LoginPage />,
+        
+//         children: [
+//           {
+//             path: "home",
+//             element: <Home />,
+            
+//           },
+//         ],
+//       },
+//     ]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Router>
-      <App></App>
-  </Router>
+      //<RouterProvider router={router} />
+      <BrowserRouter><App/></BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
