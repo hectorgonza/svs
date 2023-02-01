7cfebe263cabc59d62b3802c29189d8612ecfafd
hector gonzalez campos <hector-99hgc@hotmail.com>
2023-02-01 23:39:16 +0100

    routing fixed

--- frontend/src/Metamask/LogoutPage.js
+++ frontend/src/Metamask/LogoutPage.js
@@ -2,23 +2,25 @@ import { Col, Row, Button } from "react-bootstrap";
 
 export default function LogOut() {
 
-
+    const handleClick = async () => {
+        if(window.ethereum) {
+          await window.ethereum.enable();
+          window.ethereum.selectedAddress = null;
+          localStorage.removeItem('loginToken');
+          window.window.location.reload();
+           
+        }
+            
+    }
         return(
 
 
         <Row className="show-grid" style={{position: 'absolute', top: '10px', right: '10px'}}>
             <Col xs={12}>
-                <Button onClick={() =>{
-                localStorage.removeItem('loginToken');
-                window.location.reload();
-                }}>Log out</Button>
+            <Button onClick={handleClick}>Logout</Button>
+  
             </Col>
         </Row>
 
-
-
         )
-
-
-
 }
--- frontend/src/Views/Home.js
+++ frontend/src/Views/Home.js
@@ -1,6 +1,6 @@
 import {  Row, Col, Button } from 'react-bootstrap';
 import LogOut from '../Metamask/LogoutPage';
-import { useNavigate, Link} from 'react-router-dom';
+import { useNavigate} from 'react-router-dom';
 export default function Home() {
     
      let navigate = useNavigate()
