27ee481e7d9191e7fa2c72410af1e13199e3c12f
hector gonzalez campos <hector-99hgc@hotmail.com>
2023-07-11 19:10:59 +0200

    Beta 3.0

--- frontend/src/Views/Home.js
+++ frontend/src/Views/Home.js
@@ -1,9 +1,26 @@
 import {  Row, Col, Button } from 'react-bootstrap';
+import React from 'react';
 import LogOut from '../Metamask/LogoutPage';
-import { useNavigate} from 'react-router-dom';
+import { useNavigate,useLocation} from 'react-router-dom';
+import Alert from 'react-bootstrap/Alert';
+
+
 export default function Home() {
-    
-     let navigate = useNavigate()
+  const location = useLocation();
+  const [showAlert, setShowAlert] = React.useState(false);
+  let navigate = useNavigate()
+
+  React.useEffect(() => {
+    if (location.state && location.state.showAlert) {
+      setShowAlert(true);
+      location.state.showAlert = false;
+      window.history.replaceState(null, '');
+    }
+  }, [location.state]);
+  
+  const handleAlertClose = () => {
+    setShowAlert(false);
+  };
     return (
         
        
@@ -11,6 +28,7 @@ export default function Home() {
           
         <LogOut></LogOut>
           <Row className="show-grid text-center" style={{height: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+          
           <Col xs={12}>
             <Row>
               <Col xs={12} style={{ marginBottom: '20px'}}>
@@ -22,6 +40,9 @@ export default function Home() {
               <Col xs={12}>
                 <Button onClick={() => navigate("/results")}>Results</Button>
               </Col>
+
+              {showAlert && <Alert  variant="primary" onClose={handleAlertClose} dismissible style={{position: 'absolute', bottom: '0', left: '0'
+                , margin: '10px', width: '400px'  }}>   Successfully done!</Alert>}
             </Row>
           </Col>
         </Row>
